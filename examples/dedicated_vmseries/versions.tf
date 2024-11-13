619d688804160158ce67c6d8422b04584477d584
reyesgar <66829791+reyesgar@users.noreply.github.com>
2024-11-13 14:01:17 -0500

    Update versions.tf

--- examples/dedicated_vmseries/versions.tf
+++ examples/dedicated_vmseries/versions.tf
@@ -1,8 +1,9 @@
 terraform {
-  required_version = ">= 1.2, < 2.0"
+  required_version = "> 1.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
     random = {
       source = "hashicorp/random"

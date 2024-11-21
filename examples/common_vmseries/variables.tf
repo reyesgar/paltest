1f61701d1a64b4dadcdfffc67850ed98e164d042
reyesgar <66829791+reyesgar@users.noreply.github.com>
2024-11-21 09:27:11 -0500

    Update variables.tf

--- examples/common_vmseries/variables.tf
+++ examples/common_vmseries/variables.tf
@@ -294,7 +294,7 @@ variable "bootstrap_storage" {
 }
 
 variable "vmseries" {
-  "fw-in-1" = {
+  "fw-1" = {
       name                 = "inbound-firewall-01"
       add_to_appgw_backend = true
       bootstrap_storage = {
@@ -322,7 +322,7 @@ variable "vmseries" {
         }
       ]
     }
-    "fw-in-2" = {
+    "fw-2" = {
       name                 = "inbound-firewall-02"
       add_to_appgw_backend = true
       bootstrap_storage = {
@@ -350,61 +350,7 @@ variable "vmseries" {
         }
       ]
     }
-    "fw-obew-1" = {
-      name = "obew-firewall-01"
-      bootstrap_storage = {
-        name                   = "bootstrap"
-        static_files           = { "files/init-cfg.txt" = "config/init-cfg.txt" }
-        template_bootstrap_xml = "templates/bootstrap_obew.tmpl"
-      }
-      vnet_key = "transit"
-      avzone   = 1
-      interfaces = [
-        {
-          name       = "mgmt"
-          subnet_key = "management"
-          create_pip = true
-        },
-        {
-          name              = "private"
-          subnet_key        = "private"
-          load_balancer_key = "private"
-        },
-        {
-          name       = "public"
-          subnet_key = "public"
-          create_pip = true
-        }
-      ]
-    }
-    "fw-obew-2" = {
-      name = "obew-firewall-02"
-      bootstrap_storage = {
-        name                   = "bootstrap"
-        static_files           = { "files/init-cfg.txt" = "config/init-cfg.txt" }
-        template_bootstrap_xml = "templates/bootstrap_obew.tmpl"
-      }
-      vnet_key = "transit"
-      avzone   = 2
-      interfaces = [
-        {
-          name       = "mgmt"
-          subnet_key = "management"
-          create_pip = true
-        },
-        {
-          name              = "private"
-          subnet_key        = "private"
-          load_balancer_key = "private"
-        },
-        {
-          name       = "public"
-          subnet_key = "public"
-          create_pip = true
-        }
-      ]
-    }
-  
+ 
 
 # Application Gateway
 variable "appgws" {
